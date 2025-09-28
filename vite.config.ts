52478908e3d8a52b929941d7313861528a6ea9a0
HysamD <hysamdarwan9@gmail.com>
2025-09-28 04:35:24 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      injectRegister: "auto",
       includeAssets: ["favicon.ico", "icons/icon-192x192.png", "icons/icon-512x512.png"],
       manifest: {
         name: "Amana Supermarket Auditing App",
@@ -17,8 +18,8 @@ export default defineConfig({
         display: "standalone",
         start_url: "/",
         icons: [
-          { "src": "icons/icon-192x192.png", "sizes": "192x192", "type": "image/png" },
-          { "src": "icons/icon-512x512.png", "sizes": "512x512", "type": "image/png" }
+          { src: "icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
+          { src: "icons/icon-512x512.png", sizes: "512x512", type: "image/png" }
         ]
       }
     })
